524251eb41e734511b0f4d147bbddca000c86e82
yama bico <38546765+yamabicoforte@users.noreply.github.com>
2021-09-21 10:38:56 +0900

    :+1:update

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,42 @@ import "./styles.css";
 
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Todo Management</h1>
-    </div>
+    <>
+      <h1>TODO管理</h1>
+      <div>
+        <input placeholder="TODOを追加" type="text" />
+        <button>追加</button>
+      </div>
+      <div>
+        <h3>未完了のTODO一覧</h3>
+        <ul>
+          <li>
+            <div>
+              <p>TODO</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p>TODO</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>完了済みTODO一覧</h3>
+        <ul>
+          <li>
+            <div>
+              <p>TODO</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
